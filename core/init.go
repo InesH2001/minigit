df75e1de4a6e41878b440f3648f8cf9bde93c1a2
Liliane <lilianemezani99@gmail.com>
2025-05-19 10:19:46 +0200

    refacto : add blob, tree, commit filds for a great organisation in objects

--- core/init.go
+++ core/init.go
@@ -15,6 +15,9 @@ func InitRepo() error {
 
 	dirs := []string{
 		filepath.Join(miniGitPath, "objects"),
+		filepath.Join(miniGitPath, "objects/blob"),
+		filepath.Join(miniGitPath, "objects/commit"),
+		filepath.Join(miniGitPath, "objects/tree"),
 		filepath.Join(miniGitPath, "refs", "heads"),
 	}
 
@@ -25,7 +28,7 @@ func InitRepo() error {
 	}
 
 	headPath := filepath.Join(miniGitPath, "HEAD")
-	if err := os.WriteFile(headPath, []byte("ref: refs/heads/master"), 0644); err != nil {
+	if err := os.WriteFile(headPath, []byte("ref: refs/heads/main"), 0644); err != nil {
 		return fmt.Errorf("error creating HEAD file: %v", err)
 	}
 
